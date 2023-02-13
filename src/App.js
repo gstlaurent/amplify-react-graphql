68035a97fd3c6b5bea5b3fde6ac8a3cd6a56ffc5
Graham St-Laurent <gstlaurent@gmail.com>
2023-02-12 22:53:42 -0800

    Add toggle to only show wardrobe when selected

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
+import { useState } from "react";
+
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
 import {
   Button,
   Heading,
+  ToggleButton,
+  ToggleButtonGroup,
   View,
   withAuthenticator,
 } from '@aws-amplify/ui-react';
 import { Wardrobe } from './wardrobe';
+import React from "react";
 
 
 const App = ({ signOut }) => {
+  const [page, setPage] = useState('wardrobe');
+
   return (
     <View className="App">
       <Heading level={1}>My Clothing App</Heading>
-      <Wardrobe />
+      <div className="page-toggle">
+        <ToggleButtonGroup
+          value={page}
+          isExclusive
+          onChange={(value) => setPage(value)}
+        >
+          <ToggleButton value="outfit">Outfit</ToggleButton>
+          <ToggleButton value="wardrobe">Wardrobe</ToggleButton>
+        </ToggleButtonGroup>
+      </div>
+      {page === 'wardrobe' && <Wardrobe />}
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
 };
 
 export default withAuthenticator(App);
+
