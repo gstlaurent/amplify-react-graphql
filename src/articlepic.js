b639739ef371e7eed6195c8d1d90b99ec1502116
Graham St-Laurent <gstlaurent@gmail.com>
2023-02-21 00:44:51 -0800

    Add article refresh button (does nothing)

--- src/articlepic.js
+++ src/articlepic.js
@@ -1,17 +1,29 @@
+import './styles.css';
 import {
     Image,
+    Button
 } from '@aws-amplify/ui-react';
 
 
 export const ArticlePic = ({ article }) => {
     return (article &&
-        <Image
-            key={article.id}
-            src={article.imageUrl}
-            alt={article.usage}
-            title={article.usage}
-            style={{ width: 125 }}
-        />
+        <div className="outfit-article">
+            <Image
+                className='pic'
+                key={article.id}
+                src={article.imageUrl}
+                alt={article.usage}
+                title={article.usage}
+                style={{ width: 125 }}
+            />
+            <Button
+                className="article-refresh"
+                size="small"
+                onClick={() => console.log(`Article Refresh clicked: ${article.usage}`)}
+            >
+                🔄
+            </Button>
+        </div>
     );
 
 }
