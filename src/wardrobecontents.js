341b1aae53b4a7683da1f1b99b9ee00e7fa604e4
Graham St-Laurent <gstlaurent@gmail.com>
2023-02-26 20:01:31 -0800

    UI filters the wardrobe contents based on selected Seasons

--- src/wardrobecontents.js
+++ src/wardrobecontents.js
@@ -6,6 +6,8 @@ import {
     Image,
     Text,
     Card,
+    ToggleButtonGroup,
+    ToggleButton
 } from '@aws-amplify/ui-react';
 import { USAGES } from "./usage";
 import { deleteArticle } from "./api";
@@ -30,51 +32,64 @@ const WardrobeContents = ({ articles, setArticles }) => {
     );
     const articlesByUsage = groupBy(filteredArticles, "usage", USAGES);
     return (
-        <Expander type="single" isCollapsible={true}>
-            {USAGES.map((usage) => (
-                <ExpanderItem title={usage.plural_label} value={usage} key={usage}>
-                    <Collection
-                        items={articlesByUsage?.[usage]}
-                        type="list"
-                        direction="row"
-                        wrap="nowrap"
-                        overflow="auto"
-                    >
-                        {(article) => (
-                            <div key={article.id}>
-                                <Card
-                                    variation="elevated"
-                                    minWidth="125px"
-                                    width="125px"
-                                    height="95%">
-                                    <Flex direction="column" justifyContent="space-between" height="100%">
-                                        <Flex direction="column" justifyContent="flex-start" gap="0">
-                                            <Text as="span">{article.seasons.map(s => s.emoji)}</Text>
-                                            <Image
-                                                src={article.imageUrl}
-                                                alt={article.usage.label}
-                                            />
+        <Flex direction="column" alignItems="center">
+            <h1 style={{ margin: "0" }}><b>My Articles</b></h1>
+            <ToggleButtonGroup
+                size="large"
+                value={filteredSeasons}
+                onChange={(value) => setFilteredSeasons(value)}
+            >
+                {SEASONS.map((season) => (
+                    <ToggleButton key={season} value={season} title={season.label}>{season.emoji}</ToggleButton>
+                ))}
+            </ToggleButtonGroup>
+            <Expander type="single" isCollapsible={true}>
+                {USAGES.map((usage) => (
+                    <ExpanderItem title={usage.plural_label} value={usage} key={usage}>
+                        <Collection
+                            items={articlesByUsage?.[usage]}
+                            type="list"
+                            direction="row"
+                            wrap="nowrap"
+                            overflow="auto"
+                        >
+                            {(article) => (
+                                <div key={article.id}>
+                                    <Card
+                                        variation="elevated"
+                                        minWidth="125px"
+                                        width="125px"
+                                        height="95%">
+                                        <Flex direction="column" justifyContent="space-between" height="100%">
+                                            <Flex direction="column" justifyContent="flex-start" gap="0">
+                                                <Text as="span">{article.seasons.map(s => s.emoji)}</Text>
+                                                <Image
+                                                    src={article.imageUrl}
+                                                    alt={article.usage.label}
+                                                />
+                                            </Flex>
+                                            <Button
+                                                variation="link"
+                                                size="small"
+                                                title={`Delete ${usage.label} Article`}
+                                                onClick={() => {
+                                                    if (window.confirm(`Are you sure you want to delete this ${article.usage.label}?`)) {
+                                                        deleteArticleFromWardrobe(article, articles, setArticles);
+                                                    }
+                                                }}>
+                                                ❌
+                                            </Button>
                                         </Flex>
-                                        <Button
-                                            variation="link"
-                                            size="small"
-                                            title={`Delete ${usage.label} Article`}
-                                            onClick={() => {
-                                                if (window.confirm(`Are you sure you want to delete this ${article.usage.label}?`)) {
-                                                    deleteArticleFromWardrobe(article, articles, setArticles);
-                                                }
-                                            }}>
-                                            ❌
-                                        </Button>
-                                    </Flex>
-                                </Card>
-                            </div>
-                        )}
-                    </Collection>
-                </ExpanderItem>
-            ))
-            }
-        </Expander >
+                                    </Card>
+                                </div>
+                            )}
+                        </Collection>
+                    </ExpanderItem>
+                ))
+                }
+            </Expander >
+        </Flex>
+
     );
 };
 
